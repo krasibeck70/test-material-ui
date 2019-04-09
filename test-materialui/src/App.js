7cd1d123c10e3a5ea1c752f0f1e1f00fabff7c21
Rusev <Krasimir.Rusev@verint.com>
2019-04-09 17:06:05 +0300

    first test navbar

---
+++ test-materialui/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { green } from '@material-ui/core/colors';
+
+const styles = {
+  root: {
+    width: '100%',
+    position: 'fixed',
+    backgroundColor: 'black'
+  },
+  colorSVGs: {
+    color: "green"
+  }
+};
+
+class SimpleBottomNavigation extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation
+        value={value}
+        onChange={this.handleChange}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon className={classes.colorSVGs}/>} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon className={classes.colorSVGs}/>} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon className={classes.colorSVGs}/>} />
+      </BottomNavigation>
+    );
+  }
+}
+
+SimpleBottomNavigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleBottomNavigation);
